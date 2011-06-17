29aca60c773f3d38df923a39d25350acdb145b07
Andy Spencer <andy753421@gmail.com>
2011-06-17 06:45:27 +0000

    Update paths for lug server

--- push.sh
+++ push.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 user=spenceal
-dest=/home/lug/htdocs/proj/aweather/web/
-rsync -uavz \
-	.htaccess $user@lug.rose-hulman.edu:$dest
-rsync -uavz \
-	--delete \
+dest="/home/lug/htdocs/proj/aweather/"
+rsync -uavz .htaccess \
+	$user@lug.rose-hulman.edu:$dest
+rsync -uavz --delete \
 	--exclude '*~' \
 	--exclude '.*' \
-	--exclude '*.o' \
-	./ $user@lug.rose-hulman.edu:$dest
+	index.cgi images screens data \
+	$user@lug.rose-hulman.edu:$dest
